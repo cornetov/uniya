3e1df81b569650cdc81a87bf4747062a926b6fd8
OGolikov <ogolikov@a-practic.ru>
2019-04-23 14:17:31 +0300

    0.1.6

--- packages/uniya-xml/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-/*!
- */
-export { XmlNodeType, XmlArgumentError, XmlFormatError, XmlUnexpectedError, XmlNodeSet, XmlNode } from "./XmlNode";
-export { XmlReaderOptions, XmlReader } from "./XmlReader";
-export { XmlWriter } from "./XmlWriter";
-export { XmlTextReader } from "./XmlTextReader";
-export { XmlTextWriter } from "./XmlTextWriter";
--- packages/uniya/src/Localizer.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class L {
-    static readonly IncorrectArgument: string;
-    static readonly IncorrectFormat: string;
-    static readonly UnexpectedName: string;
-    static readonly UnknownName: string;
-    static readonly AuthenticationFail: string;
-    static readonly StorageFail: string;
-}
